0e6dad10d93fe70a61029e0c24774414d06e8b04
keef hassan <keefhassan4@gmail.comgit config --global user.email keefhassan4@gmail.coome>
2024-02-24 12:58:02 -0600

    create routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,44 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
+import { HashRouter as Router, Route } from "react-router-dom";
+
+import React from "react";
+import axios from "axios";
+import "./App.css";
+import Header from "../Header/header";
+import Feeling from "../Feeling/feeling";
+import Understanding from "../Understanding/understanding";
+import Support from "../Support/support";
+import Comments from "../Comments/comments";
+import Review from "../Review/review";
+import Success from "../Success/success";
 
 function App() {
-
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route path="/" exact>
+          <Feeling />
+        </Route>
+        <Route path="/understanding">
+          <Understanding />
+        </Route>
+        <Route path="/support">
+          <Support />
+        </Route>
+        <Route path="/comments">
+          <Comments />
+        </Route>
+        <Route path="/review">
+          <Review />
+        </Route>
+        <Route path="/success">
+          <Success />
+        </Route>
+
+
+
+      </div>
+    </Router>
   );
 }
 
